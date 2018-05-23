3f65d4648d9ec95a503868004bfafe5dd7e14507
Tim Safronov <tims@windowslive.com>
2018-05-24 00:06:34 +0300

    Commit

---
+++ BC.Bootstrap/DomainProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BC.Data.Entity.Authors;
+using BC.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BC.Bootstrap
+{
+    public class DomainProfile : Profile
+    {
+        public DomainProfile()
+        {
+            CreateMap<AuthorEM, AuthorVM>();
+        }
+    }
+}
