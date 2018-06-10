c9ddadafdaee70b58173e7bcc7ca3e287a39ed24
Tim Safronov <38751885+timsdp@users.noreply.github.com>
2018-06-11 02:07:07 +0300

    Knockout experiments

--- BC.UI.Web/wwwroot/js/view-model/author-vm.js
+++ BC.UI.Web/wwwroot/js/view-model/author-vm.js
@@ -1,20 +1,41 @@
-var AuthorVMInstance = new AuthorVM();
+var AuthorVM = new AuthorVM();
 
 function AuthorVM() {
-    var self=this;
+    var self = this;
+    self.UrlSaveVm = '';
+    self.UrlGetVm = '';
+
+
+    //Observable fields
     self.FirstName = ko.observable("Jack");
     self.LastName = ko.observable("London"); 
     self.FullName = ko.pureComputed(function () { return self.FirstName() + " " + self.LastName();});
     self.Country = ko.observable("USA");
     self.Quote = ko.observable("Famous authors quote value");
-    self.BooksCount = ko.observable("28");
-    self.Born = ko.observable("1865");
-    self.Died = ko.observable("1930");
+    self.BooksCount = ko.observable(28);
+    self.Born = ko.observable(1865);
+    self.Died = ko.observable(1930);
     self.TopBooks = ["Book name 1", "Book name 2", "Book name 3", "Book name 4"];
+
     self.IsFavourite- ko.observable(true);
     self.Wiki = ko.observable("https://en.wikipedia.org/wiki/Jack_London");
 
-    self.InitVM = function () {
+    //Initialization
+    //self.InitVM = function () {
+    //    console.log("Getting Author from server...");
+    //    console.log("json: ");
+    //}
 
+    self.Save = function () {
+        console.log("Post Author to server...");
+        var dto = ko.toJS(self);
+        console.log("json: " + dto);
+        $.ajax({
+            url: self.UrlSaveVm,
+            data: ko.toJS(self),
+            type: 'POST',
+            contentType: 'application/x-www-form-urlencoded'
+        }).success(console.log('Save: success!')).error(console.log('Save: error!'));
     }
+
 }
